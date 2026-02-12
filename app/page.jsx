4a353f8fd6dcf389f1b755796fb993e63a558e63
ravijasywal69-lang <ravijasywal69@gmail.com>
2026-02-12 16:46:49 +0530

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{padding:"40px",fontFamily:"Arial"}}>
+      <h1>KHUSHI COLLECTION</h1>
+      <h2>Premium Hair Rubber Bands</h2>
+      <p>Strong Elastic | No Hair Damage | Stylish & Affordable</p>
+
+      <h3>Products</h3>
+      <ul>
+        <li>Pack of 2 – ₹99</li>
+        <li>Pack of 5 – ₹199</li>
+        <li>Pack of 10 – ₹349</li>
+      </ul>
+    </div>
+  );
+}
